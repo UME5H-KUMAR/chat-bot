0740e5d51c2d3f3c39f5480a5bcddbda1de8b290
UME5H-KUMAR <beingtomato7@gmail.com>
2026-04-16 01:27:42 +0530

    2. used multiple models with respective chat clients

--- src/main/java/com/dev/tomato/chat_bot/controller/ChatController.java
+++ src/main/java/com/dev/tomato/chat_bot/controller/ChatController.java
@@ -1,9 +1,14 @@
 package com.dev.tomato.chat_bot.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.var;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,18 +17,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping
 public class ChatController {
 
-    private ChatClient chatClient;
+    private ChatClient openAiChatClient;
+    private ChatClient ollamaChatClient;
 
-    public ChatController(ChatClient.Builder builder){
-        chatClient= builder.build();
+    public ChatController(OpenAiChatModel openAiChatModel, OllamaChatModel ollamaChatModel){
+        this.openAiChatClient = ChatClient.builder(openAiChatModel).build();
+        this.ollamaChatClient= ChatClient.builder(ollamaChatModel).build();
+        // chatClient= ChatClient.builder(chatModel).build();
     }
 
 
     @GetMapping("/chat")
-    public ResponseEntity<String> chat(@RequestParam(value="q") String q){
+    public ResponseEntity<String> chat(@RequestParam(value="query") String query){
 
 
-        String response= chatClient.prompt(q)
+        var response= openAiChatClient.prompt(query)
                 .call()
                 .content();
         return ResponseEntity.ok(response);
